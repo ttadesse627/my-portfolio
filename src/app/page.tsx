7f538be2ecd2dda86d4c8845df9b283a09cce756
ttadesse627 <ttadesse627@gmail.com>
2025-02-08 01:20:22 +0300

    Fix Navbar import paths in layout files for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Head from "next/head";
 export default function Home() {
   return (
--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 
 
 export const metadata: Metadata = {
